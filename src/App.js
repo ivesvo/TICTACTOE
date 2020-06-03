44ccbe657519ee8aa5dd18f776ee1877169b0880
ivy <ivyvodesign@gmail.com>
2020-06-03 16:56:48 +0700

    wednesday homework

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
 import Board from './components/Board'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FacebookLogin from 'react-facebook-login';
+
+
 import './App.css'
 import './fonts/Mixo.ttf'
-import Container from 'react-bootstrap/Container'
 
 export default class App extends Component {
   constructor(props){
     super(props)
     this.state={
-      // history: [{
-        squares: Array(9).fill(null),
-      // }],
+      history: [
+     
+      ],
+      squares: Array(9).fill(null),
       isXNext: true, // if its true then X , false then O 
-      gameOver: false
+      winner: null,
+      topRank : null,
+      user: null,
+      
+      
 
     };
   }
@@ -22,30 +34,80 @@ export default class App extends Component {
 
   timeTravel = (id)=> {
     console.log("b2b", id)
-    
+    this.setState({squares:this.state.history[id].squares.slice(), isXNext: this.state.history[id].isXNext});
 
+  
   }
+
+
+  responseFacebook =(data)=>{
+
+    this.setState({...this.state,user:data.name})
+    console.log(this.state.user)
+  }
+
   render() {
     return (
       <div>
-        <div>
+        
+        <Row className="justify-content-md-center">
         <h1>¯\_(ツ)_/¯  CAMEL VS DINOSAURS ¯\_(ツ)_/¯ </h1>
-        </div>
-        <Container>
-        <div className="game">
-          <Board {...this.state} setTheState={this.setTheState}/>  {/* <Board square={this.state.squares} isXNext={this.state.isXNext}/> */}
-          </div>
+        {
+          (this.state.user)
+          ? this.state.user.name
+          :  <FacebookLogin
+          autoLoad={false}
+          appId="206175450381158"
+          fields="name,email,picture"
+          callback={(resp) => this.responseFacebook(resp)}
+        />
+        }
+        
+        </Row>
 
-        </Container>
+        <Row>
+        <Col>
+          <Container className="history">
+          <div className="toprank">
+            <h2>TOP FIGHTER:</h2>
+              {this.state.topRank!==null ?this.state.topRank.map (item => {
+                      return(
+                      <div>{item.player}, {item.score}</div>
+                      )
+                  }) :"" }
+          </div>
           
-        {/* <div className="history">
-         {this.state.history.map ((item,index) =>{
-           return <div><button onClick={()=>this.timeTravel(index)}>Move {index+1}</button></div>
-         })}
-        </div> */}
-        <div className="bottom">
+          </Container>
+        </Col>
+        
+        <Col>
+          <Container fluid>
+          <div className="game">
+            <Board {...this.state} setTheState={this.setTheState} />  {/* <Board square={this.state.squares} isXNext={this.state.isXNext}/> */}
+            </div>
+          </Container>
+          <div className="bottom">
           <img src="https://alienmelon.gamejolt.io/images/IMG_FIRE01.gif"/>
         </div>
+          </Col>
+          <Col>
+            <div className="history">
+          {this.state.history.map ((item,index) =>{
+            return <Button className="button" variant="light" onClick={()=>this.timeTravel(index)}>Move {index+1}</Button>
+          })}
+          </div>
+          </Col>
+          
+
+        </Row>
+       
+         
+
+      
+        
+          
+       
+        
       </div>
     )
   }
--- src/components/Board.js
+++ src/components/Board.js
@@ -4,14 +4,6 @@ import X from './X.png'
 import O from './O.png'
 
 
-// export const Game = {
-//     X:{
-//         ul: 
-//     }
-//     O:{
-
-//     }
-
 
 export default class Board extends Component {
     renderSquare = (num) => {
@@ -21,17 +13,21 @@ export default class Board extends Component {
     boxClick = (id) => {
         // console.log("what is IsNext", this.props.isXnext)
         // console.log("clicked box id is", id)
-        
+        if (this.props.winner){
+            
+            return;
+        } 
         let squaresApp = this.props.squares
         if (this.props.squares[id] === null){
             squaresApp[id] = this.props.isXNext? X:O
         } else {
             alert("Choose different land!")
+            return;
+            
         }
        
-        this.props.setTheState({square:squaresApp, isXNext:!this.props.isXNext})
-        // history:[...this.props.history.slice(),{square:squaresApp, isXNext:!this.props.isXNext}]
-
+        this.props.setTheState({squares:squaresApp, isXNext:!this.props.isXNext, history:[...this.props.history.slice(),{squares:squaresApp.slice(), isXNext:!this.props.isXNext}]})
+        this.calculateWinner(this.props.squares);
     };
     
     calculateWinner = (squares) =>{
@@ -44,32 +40,77 @@ export default class Board extends Component {
             [2,5,8],
             [2,4,6],
             [0,4,8],
-        ];
+        ]
         for (let i = 0; i< lines.length; i++){
             const [a,b,c] = lines[i];
             if (squares[a] && squares[a] === squares[b] && squares [a] === squares[c]){
-                return squares[a];
+                this.props.setTheState({winner:squares[a]})
+                this.postData()
+                
+                return;
             }
         }
         return null
     };
+    
+    postData = async() =>{
+
+    let data = new URLSearchParams();
+    console.log("player",this.props.user, "score")
+    data.append("player", this.props.user);
+    data.append("score", '⌛');
+    const url = `http://ftw-highscores.herokuapp.com/tictactoe-dev`;
+    const response = await fetch(url, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: data.toString(),
+    json: true
+}
+    );
+
+    console.log(response.items)
+}
+    getData= async()=>{
+        const url = `http://ftw-highscores.herokuapp.com/tictactoe-dev`
+        let res = await fetch(url)
+        let data = await res.json()
+        // console.log("data is", data)
+        this.props.setTheState({
+            topRank:[...data.items]
+        })
+
+}
+    componentDidMount =()=> {
+        this.getData();
+    }
+        
     render() {
         
-        let status =''
-        let winner = this.calculateWinner(this.props.squares);
+        let status ='';
+        let winner = this.props.winner;
         if (winner){
         // status = <div> Winner is  <img src={this.props.squares}/> </div>
-            status = `Winner is ` + winner;
+        status = <div>WINNER IS  <img src={this.props.winner} width="60"/>  GAME OVER! </div>
         } else {
-            console.log(X)
+           
             status = <div>NEXT FIGHTER IS‿︵‿︵‿︵‿{this.props.isXNext? <img src={X} width="60"/> : <img src={O} width="60"/>}‿︵‿︵‿︵‿</div>
         }
+        console.log(this.props.topRank)
 
         return (
             <div>
 
                 <h2>{status}</h2>
-              
+              {/* {this.props.topRank!==null ?this.props.topRank.map (item => {
+                  return(
+                  <div>{item.player}, {item.score}</div>
+                  )
+              }) :"" } */}
+              {/* {
+                  this.state.topRank ?"aaaaaaaaa" :"bbbb"
+              } */}
                 <div className="row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
